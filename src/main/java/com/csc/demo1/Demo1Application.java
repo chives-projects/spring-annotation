b9d05f1714d29d7ab7de87c7bdd1f2174a9e57eb
chenshichao <chenshichao@eastmoney.com>
2020-11-25 10:58:12 +0800

    del DisposableBean

--- src/main/java/com/csc/demo1/Demo1Application.java
+++ src/main/java/com/csc/demo1/Demo1Application.java
@@ -2,23 +2,10 @@ package com.csc.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class Demo1Application {
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Demo1Application.class, args);
-
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            if ("exit".equals(scanner.nextLine())){
-                break;
-            }
-        }
-
-        SpringApplication.exit(applicationContext);
-
+        SpringApplication.run(Demo1Application.class, args);
     }
 }
