3ce2c3557208a0a318f9bd2ab68277141ad3647b
chenshichao <chenshichao@eastmoney.com>
2020-11-28 16:28:49 +0800

    update test

--- src/test/java/com/csc/demo1/Demo1ApplicationTests.java
+++ src/test/java/com/csc/demo1/Demo1ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.csc.demo1;
 
-import org.junit.Test;
+import com.csc.demo1.controller.DemoController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class Demo1ApplicationTests {
 
+    @Autowired
+    DemoController demoController;
+
     @Test
     public void contextLoads() {
+        System.out.println(demoController.get("1"));
         System.out.println("hello");
     }
 
